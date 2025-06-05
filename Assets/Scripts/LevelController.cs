3db01595f487cd601c16d10ed643204bcc72bde9
Victor Yashin <v.yashin@carx-tech.com>
2025-06-05 16:43:13 +0300

    xlab lesson 4.3 completed

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -14,26 +14,48 @@ namespace Golf
 
         private float m_lastSpawnedTime = 0;
 
+        public int score = 0;
+        public int highScore = 0;
+
+        private List<GameObject> m_stones  = new List<GameObject>(16);
+
         private void Start()
         {
             m_lastSpawnedTime = Time.time;
             RefreshDelay();
         }
 
+        private void OnStickHit()
+        {
+            score++;
+            highScore = Mathf.Max(highScore, score);
+            Debug.Log($"score: {score} - highScore: {highScore}");
+        }
+
         private void OnEnable()
         {
-            Stone.onCollisionStone += GameOver;
+            GameEvents.onStickHit += OnStickHit;
+            score = 0;
         }
 
         private void OnDisable()
         {
-            Stone.onCollisionStone -= GameOver;
+            GameEvents.onStickHit -= OnStickHit;
         }
 
         private void GameOver()
         {
             Debug.Log("Game Over!");
             enabled = false;
+        }
+
+        public void ClearStones()
+        {
+            foreach (var stone in m_stones)
+            {
+                Destroy(stone);
+            }
+            m_stones.Clear();
         }
 
         public void RefreshDelay()
@@ -46,7 +68,8 @@ namespace Golf
         {
                 if (Time.time >= m_lastSpawnedTime + m_delay)
                 {
-                    spawner.Spawn();
+                    var stone = spawner.Spawn();
+                m_stones.Add(stone);
                     m_lastSpawnedTime = Time.time;
                 RefreshDelay();
                 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,8 +24,6 @@ namespace Golf
         {
             m_lastPosition = helper.position;
 
-            m_isDown = Input.GetMouseButton(0);
-
             Quaternion rot = stick.localRotation;
 
             Quaternion toRot = Quaternion.Euler(0, 0, m_isDown ? range : -range);
@@ -34,6 +32,12 @@ namespace Golf
 
             stick.localRotation = rot;
         }
+
+        public void SetDown(bool value)
+        {
+            m_isDown = value;
+        }
+
         public void OnCollisionStick(Collider collider)
         {
             if (collider.TryGetComponent(out Rigidbody body))
@@ -41,9 +45,10 @@ namespace Golf
                 var dir = (helper.position - m_lastPosition).normalized;
                 body.AddForce(dir * power, ForceMode.Impulse);
 
-                if (collider.TryGetComponent(out Stone stone))
+                if (collider.TryGetComponent(out Stone stone)&& !stone.isAffect)
                 {
                     stone.isAffect = true;
+                    GameEvents.StickHit();
                 }
             }
 
